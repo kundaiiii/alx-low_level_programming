9bbeb662ed8060935d4786bd0696093e65cde588
kundaiiii <kundaimm@yahoo.com>
2022-12-19 08:21:33 -0800

    main.h

--- 0x18-dynamic_libraries/main.h
+++ 0x18-dynamic_libraries/main.h
@@ -1,5 +1,8 @@
 #ifndef MAIN_H
+
 #define MAIN_H
+
+
 
 int _putchar(char c);
 
@@ -40,4 +43,7 @@ unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 
 char *_strstr(char *haystack, char *needle);
-#endif
+
+
+
+#endif /*MAIN_H*/
